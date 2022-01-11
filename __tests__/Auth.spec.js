5c14dbec7a707483e52f5af3c69ac75bde351c9e
cspark0610 <cpark0610@gmail.com>
2022-01-11 16:04:13 -0300

    auth spec. completed

--- __tests__/Auth.spec.js
+++ __tests__/Auth.spec.js
@@ -13,13 +13,18 @@ beforeEach(() => {
 	return User.destroy({ truncate: true });
 });
 describe('authentication', () => {
-	const addUser = async () => {
-		const user = { username: 'user1', email: 'user1@mail.com', password: 'P4ssword', inactive: false };
+	const validUser = { username: 'user1', email: 'user1@mail.com', password: 'P4ssword', inactive: false };
+
+	const addUser = async (user = validUser) => {
 		const hash = await bcrypt.hash(user.password, 10);
 		return User.create({ ...user, password: hash });
 	};
-	const postAuthentication = async (credentials) => {
-		return request(app).post('/api/1.0/auth').send(credentials);
+	const postAuthentication = async (credentials, options = {}) => {
+		let agent = request(app).post('/api/1.0/auth');
+		if (options.language) {
+			agent.set('Accept-Language', options.language);
+		}
+		return await agent.send(credentials);
 	};
 
 	it('returns 200 ok when user credentials, which are composed by email and password are correct', async () => {
@@ -43,6 +48,32 @@ describe('authentication', () => {
 	it('returns 401 unauthorized when user credentials are incorrect', async () => {
 		await addUser();
 		const response = await postAuthentication({ email: 'non-exists@mail.com', password: 'P4ssword' });
+		expect(response.status).toBe(401);
+	});
+
+	it.each`
+		language | message
+		${'en'}  | ${'incorrect credentials'}
+		${'es'}  | ${'credenciales incorrectas'}
+	`('returns $message when user credentials are incorrect in language $language', async ({ language, message }) => {
+		const response = await postAuthentication({ email: 'user1@mail.com', password: 'password' }, { language });
+		expect(response.body.message).toBe(message);
+	});
+
+	it('returns 401 unauthorized when password provided as credential is incorrect', async () => {
+		// aca ya estoy seteando el password de la forma que esta en el addUser
+		const response = await postAuthentication({ email: 'user1@mail.com', password: ' ' });
+		expect(response.status).toBe(401);
+	});
+
+	it('returns status 401 when logging with an inactive user account', async () => {
+		await addUser({ ...validUser, inactive: true });
+		const response = await postAuthentication({ email: 'user1@mail.com', password: 'P4ssword' });
+		expect(response.status).toBe(403);
+	});
+
+	it('returns 401 when email format its empty', async () => {
+		const response = await postAuthentication({ password: 'P4ssword' });
 		expect(response.status).toBe(401);
 	});
 });
--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const UsersService = require('../user/UserService');
+const AuthenticationException = require('./AuthenticationException');
+const ForbiddenException = require('./ForbiddenException');
+const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
 
-router.post('/api/1.0/auth', async (req, res) => {
-	const { email } = req.body;
-	const user = await UsersService.findByEmail(email);
-
-	if (!user) {
-		return res.status(401).send();
+router.post('/api/1.0/auth', check('email').isEmail(), async (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return next(new AuthenticationException());
 	}
+
+	const { email, password } = req.body;
+	const user = await UsersService.findByEmail(email);
+	if (!user) {
+		//return res.status(401).send();
+		return next(new AuthenticationException());
+	}
+	// otro requerimiento del controllador seria chequear que el password hasheado
+	// del user encontrado en db coincida con el de la credencial provista
+	// para ello se usa el metodo compare de bcrypt
+	const match = await bcrypt.compare(password, user.password);
+	if (!match) {
+		return next(new AuthenticationException());
+	}
+	if (user.inactive) {
+		return next(new ForbiddenException());
+	}
+
 	res.send({
 		id: user.id,
 		username: user.username,
